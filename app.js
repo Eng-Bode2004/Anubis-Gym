2441f52acc074d322d88e1b4caa06879eedfc4a8
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-11-18 17:24:00 +0200

    init commit

--- app.js
+++ app.js
@@ -10,15 +10,16 @@ const PORT = 5000;
 
 app.use(express.json()); // Middleware to parse JSON
 
-// Users Routes //
-const PaymentProvider_Routes = require('./Routes/Routes');
-app.use('/api/v3/payment-provider', PaymentProvider_Routes);
-
 // ✅ حل مشكلة CORS
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
+
+// Users Routes //
+const PaymentProvider_Routes = require('./Routes/Routes');
+app.use('/api/v3/payment-provider', PaymentProvider_Routes);
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
